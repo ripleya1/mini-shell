17e3864d7a525eb9391b408d2ef040714032a958
Aaron Ripley <ripleya1@xavier.edu>
2023-09-07 12:17:44 -0400

    input and tokenization finally works

--- shell.c
+++ shell.c
@@ -8,30 +8,40 @@
 int main(){
     char *inputStr;
     inputStr = malloc(200 * sizeof(char));
-    int *numTokens;
+    
     char ***tokenVect;
     tokenVect = malloc(sizeof(char**) * 200 * 200);
+    
     int c = 0;
     int cont = TRUE;
     char *whitespace = " \t\f\r\v\n";
+    
     while(cont){
         cont = FALSE;
         printf("%s", "> ");
         inputStr = fgets(inputStr, 200 * sizeof(char), stdin); // get user input
+        if(inputStr == NULL){
+            printf("%s", "Invalid input.\n");
+            return 0;
+        }
+
+        // tokenize each token and put into 2d array
         char *tokenizedStr = strtok(inputStr, whitespace);
-        // tokenize each token
+        puts(tokenizedStr);
+        tokenVect[0] = tokenizedStr;
+        puts(tokenVect[0]);
         while(tokenizedStr != NULL){
             tokenVect[c] = tokenizedStr;
-            c++;
-            tokenizedStr = strtok(inputStr, whitespace);
+            c = c + 1;
+            tokenizedStr = strtok(NULL, whitespace);
         }
     }
-    free(inputStr);
+    // free(inputStr);
 
     int i = 0;
     int j = 0;
     while(tokenVect[i] != NULL){
-        printf("%s ", tokenVect[0]);
+        puts(tokenVect[i]);
         i++;
     }
 }
