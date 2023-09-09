74e8a77402dc6b7a4d41dcd19bcfd6e90b2ede72
Aaron Ripley <ripleya1@xavier.edu>
2023-09-09 17:16:57 -0400

    document

--- shell.c
+++ shell.c
@@ -1,7 +1,11 @@
 /*
 Aaron Ripley
 9/9/23
-A mini shell.
+A mini shell. Given a command from the default PATH (usr/bin), it uses execvp to execute that command using any arguments that the user passed.
+The execution of the command is forked, creating a new process, with the child process running the command and the parent process waiting for the child to complete. 
+The shell repeatedly takes input from the user until it encounters an end of file character, done by inputting Ctrl + D. 
+Similarly to an actual shell program, enter executes the inputted command, and if nothing is inputted, the user is allowed to input a command again.
+If an error is encountered when executing the command, an error message is given using perror. 
 */
 
 #include <stdio.h>
@@ -33,7 +37,7 @@ int main(){
             int execErr;
             char *pathFromTok;
             pathFromTok = tokenv[0];
-            // rewrite token array
+            // rewrite token array so the path/command name (ie the 0th index) isn't in it
             for(int i = 1; i < tokenc; i++){
                 tokenv[i - 1] = tokenv[i];
             }
@@ -52,6 +56,12 @@ int main(){
     }
 }
 
+// gets the user input and puts it into the input string
+// preconditions: the input string has not been filled
+// params: the user input string
+// returns: none
+// postconditions: the input string is updated with the user input,
+// if the user inputs an EOF, the program exits
 void getInput(char *inputStr){
     int numTokens;
     printf("%s", "> ");
@@ -62,6 +72,11 @@ void getInput(char *inputStr){
     }
 }
 
+// tokenizes the input string and puts each token into the tokenVector array
+// preconditions: tokenVector has not been filled
+// params: the user input string and an array of strings to be updated with the tokenized version of the user input
+// returns: the number of tokens in the user input string
+// postconditions: the tokenVector array is updated with the tokenized user input, with each token being an element in the vector
 int tokenize(char *input, char ***tokenVector){
     char *whitespace = " \t\f\r\v\n";
     int c = 0;
